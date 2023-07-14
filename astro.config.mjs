c482c0f1173d9c2c8f44dfa3369ce8bdecc92c44
Almorder <137223296+Almorder@users.noreply.github.com>
2023-07-15 01:01:37 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
-import { defineConfig } from "astro/config";
+--git a/astro.config.mjs b/astro.config.mjs
+index 5d71593..f4475b5 100644
+--- a/astro.config.mjs
++++ b/astro.config.mjs
+@@ -6,5 +6,4 @@ import tailwind from "@astrojs/tailwind";
+ export default defineConfig({
+  integrations: [tailwind()],
+   site: "https://Almorder.github.io",
+-  base: "/repo",
+ });
 
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
-export default defineConfig({
- integrations: [tailwind()],
-  site: "https://Almorder.github.io",
-  base: "/repo",
-});
+--git a/tailwind.config.js b/tailwind.config.js
+index c70b548..20c416e 100644
+--- a/tailwind.config.js
++++ b/tailwind.config.js
+@@ -15,7 +15,3 @@ module.exports = {
+                }
+        }
+ };
+-
+-}
+-       plugins: [],
+-}
